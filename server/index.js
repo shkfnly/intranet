f39877e1e6b73bc74a7c6b08e5499d2b8a0a16b5
shkfnly <ashoka.finley@gmail.com>
2017-10-14 09:32:43 +0900

    add path requirement

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')))
 
 const app = express()
 app.use(bodyParser.urlencoded({
