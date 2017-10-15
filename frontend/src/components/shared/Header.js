401746b2885d5a5a4c4bf5f5eed2fa015d26cf47
shkfnly <ashoka.finley@gmail.com>
2017-10-15 14:20:32 +0900

    Add a link to the spokes db

--- frontend/src/components/shared/Header.js
+++ frontend/src/components/shared/Header.js
@@ -44,20 +44,24 @@ class Header extends React.Component {
       </Nav>)
       : (<Nav className='ml-auto' navbar style={{flexDirection: 'row'}}>
         <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
+          <NavLink href='https://polar-forest-13728.herokuapp.com/'>Spokes</NavLink>
+        </NavItem>
+        <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
           <NavLink href='/circles'>Circles</NavLink>
         </NavItem>
         <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
           <NavLink href='/map'>Map</NavLink>
         </NavItem>
         <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
-          <NavLink href='/edit'>Edit</NavLink>
+          <NavLink href='/edit'>Profile</NavLink>
         </NavItem>
         <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
-          <NavLink href='/profiles'>Profiles</NavLink>
+          <NavLink href='/profiles'>Members</NavLink>
         </NavItem>
         <NavItem style={{paddingTop: 6, paddingBottom: 6, paddingLeft: 12, paddingRight: 12}}>
           <NavLink onClick={this.props.logoutUser}>Logout</NavLink>
         </NavItem>
+
       </Nav>)
     return (
       <div>
